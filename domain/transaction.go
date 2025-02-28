02a20f3decfd7563ad5ee6e1b102d0994b19ce7e
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2025-02-28 17:54:16 +0700

    feat(payment): update transaction domain fields

--- domain/transaction.go
+++ domain/transaction.go
@@ -1,21 +1,24 @@
 package domain
 
 import (
-    "time"
-    "github.com/google/uuid"
-    "gorm.io/gorm"
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Transaction struct {
-    ID        string    `gorm:"type:uuid;primaryKey"`
-    UserID    string    `gorm:"index;not null"`
-    Amount    float64   `gorm:"not null"`
-    Currency  string    `gorm:"size:3;not null;check:currency IN ('THB')"`
-    Type      string    `gorm:"size:10;not null"`
-    CreatedAt time.Time `gorm:"autoCreateTime"`
+	ID                  string    `gorm:"type:uuid;primaryKey"`
+	UserID              string    `gorm:"index;not null"`
+	Amount              float64   `gorm:"not null"`
+	Currency            string    `gorm:"size:3;not null;check:currency IN ('THB')"`
+	Type                string    `gorm:"size:10;not null"`
+	ThirdPartyGateway   string    `gorm:"size:50;not null"`
+	ThirdPartyPaymentID *string   `gorm:"size:255"`
+	CreatedAt           time.Time `gorm:"autoCreateTime"`
 }
 
 func (t *Transaction) BeforeCreate(tx *gorm.DB) (err error) {
-    t.ID = uuid.New().String()
-    return
+	t.ID = uuid.New().String()
+	return
 }
